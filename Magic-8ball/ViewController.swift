1c36a02857b595c60492b5758c862f5098ca7efa
Akmaral <e.akmaral024@mail.ru>
2023-05-26 13:33:34 +0600

    Initial Commit

---
+++ Magic-8ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic-8ball
+//
+//  Created by Akmaral Ergesh on 26.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
